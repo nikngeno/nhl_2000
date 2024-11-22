01ff37d0da707ce42b8685c4da8da12df1682381
MiguelG194 <miguela194@outlook.com>
2024-11-21 16:48:44 -0800

    Update Miguel.sql
    Added the Events table

--- Miguel.sql
+++ Miguel.sql
@@ -10,4 +10,20 @@ create table TeamCoaches
 	constraint FK_CoachID foreign key (CoachId) references Coaches(CoachID),
 	constraint FK_SeasonID foreign key (SeasonID) references Seasons(SeasonID)
 )
-
+create table Events
+(
+	EventID int primary key,
+	GameID int,
+	EventType nvarchar(50),
+	Period int,
+	Time date,
+	PlayerID int,
+	TeamID int,
+	AssistPlayerID int null,
+	Penaltytype nvarchar(50) null,
+	PenaltyMinutes date null,
+	constraint FK_GameID foreign key (GameID) references Games(GameID),
+	constraint FK_PlayerID foreign key (PlayerID) references Player(PlayerID),
+	constraint FK_TeamID foreign key (TeamID) references Team(TeamID),
+	constraint FK_AssistPlayerID foreign key (AssistPlayerID) references Player(PLayerID)
+)
